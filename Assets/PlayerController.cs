078340f197100a4c1c1332584fc2c87116d34330
ym873932778 <873032778@qq.com>
2017-05-21 21:03:28 +0800

    YM666

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+    public float xMin, xMax, zMin, zMax;
+
+}
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    private float nexrFire;
+    public float fireRate;
+    public GameObject shot;
+    public Transform shotSqawn;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButton("Fire1") && Time.time > nexrFire) { 
+            nexrFire = Time.time + fireRate;
+            Instantiate(shot, shotSqawn.position, shotSqawn.rotation);
+            GetComponent<AudioSource>().Play();
+        }
+		
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+       
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody> ().velocity = movement * speed;
+
+        GetComponent<Rigidbody> ().position = new Vector3(
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+            );
+
+
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f,
+                                                                0.0f,
+                                                                GetComponent<Rigidbody>().velocity.x * -tilt);
+    }
+}
